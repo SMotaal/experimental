5be50b42b9949427baa02b3a3f0a6ef1c7fcd43d
Saleh <saleh@grasppe.com>
2018-10-12 06:31:55 -0700

    Add fragma alpha
    Responsive DOM using pure Web Components

---
+++ fragma/alpha/README.md
@@ -0,0 +1,21 @@
+# Fragma (*concept*)
+
+Okay, concept means messy and probably requires flags on nightly or the likes, agreed?
+
+**Works**
+
+> *Chrome*
+> <samp>Version 69.0.3497.100 (Official Build) (64-bit)</samp>
+
+> *Firefox Nightly*
+> <samp>64.0a1 (2018-10-06) (64-bit)</samp>
+
+> *Safari*
+> <samp>Version 12.0 (14606.1.36.1.9)</samp>
+> - Using own fallback for IntersectionObserver
+
+**Not Yet!**
+
+> *Safari Technology Preview*
+> - IntersectionObserver does not work yet!
+
---
+++ fragma/alpha/components/component-a.js
@@ -0,0 +1,193 @@
+import {
+  html,
+  idle,
+  cancelIdle,
+  noop,
+  Component,
+  viewPort,
+  indeterminate,
+} from '../lib/helpers.js';
+
+let instance = 0;
+
+const apply = Reflect.apply;
+
+export default class ComponentA extends Component {
+  constructor() {
+    super();
+    const root = this.attachShadow ? this.attachShadow({mode: 'open'}) : this;
+
+    const tillViewable = indeterminate(this, 'wasViewable', (value, resolve) => resolve(!!value));
+
+    this.messageText = '';
+
+    const message = {toString: () => this.messageText};
+
+    this.initialized = new Promise(resolve => {
+      viewPort.observe(this);
+
+      /// Fragments
+      // <style>@import "${{toString: () => styles.href}}";</style>
+      const fragment = (this.fragment = html`
+        <style>
+          .fill {
+            --fill: transparent;
+          }
+
+          .grid {
+            display: grid;
+            place-items: center;
+            grid-auto-flow: column;
+          }
+
+          red {
+            color: red;
+          }
+
+          green {
+            color: green;
+          }
+
+          .content::after {
+            content: ' ';
+          }
+
+          /* :host(.viewable) .content::after { */
+          .content::after {
+            content: attr(data-text);
+          }
+
+          :host > * {
+            --text: transparent;
+            background-color: var(--fill, #f006);
+            color: var(--text, currentcolor);
+            transition: color 250ms 5ms, background 250ms 5ms;
+          }
+
+          :host(.viewable) > * {
+            --text: orange;
+          }
+
+          #message {
+            background-color: green;
+          }
+
+        </style>
+        <!-- <div name="time-only">${time}</div> -->
+        <div class="fill" style="display: grid; place-items: center; grid-auto-flow: column; grid-auto-columns: 1fr;">
+          <div name="instance-only">${++instance}</div>
+          <div class="content" data-text="${time}">&nbsp;</div>
+          <div id=message>${message}</div>
+          <div id=field>
+            <slot name="field" oninput="component.elements.message.textContent = this.children[0].value"><input value="${instance}"/></slot>
+          </div>
+        </div>
+      `);
+      const slots = (this.slots = [...fragment.querySelectorAll('slot')]);
+      const elements = (this.elements = [...fragment.querySelectorAll('[id]')]);
+
+      /// Slots
+      this.addEventListener('slotchange', this.slotChangeCallback);
+      for (const slot of slots) {
+        slot.component = this;
+        slots[slot.name] = slot;
+      }
+
+      /// Elements
+
+      for (const element of elements) {
+        element.component = this;
+        elements[element.id] = element;
+      }
+
+      this.initialize = async () => {
+        try {
+          this.initialize = noop;
+          await fragment.ready;
+          await Promise.race([tillViewable, new Promise(resolve => idle(() => resolve()))]);
+          root.appendChild(fragment);
+          resolve(this);
+          await tillViewable;
+
+          let updating;
+          const update = async () => {
+            if (updating) return;
+            (updating = true), fragment.update();
+            await fragment.updated, (updating = false);
+          };
+
+          let pending;
+          setInterval(() => {
+            cancelIdle(pending);
+            if (this.viewable) update();
+            else if (!updating) pending = idle(update);
+          }, 1000);
+
+          this.style.opacity = 1;
+        } catch (exception) {
+          console.warn(exception);
+        }
+      };
+    });
+  }
+
+  handleEvent(event, source) {
+    const type = event && event.type;
+    if (!type) return;
+
+    if (event.composed && !source) {
+      const path = (event.composed && event.path) || event.composedPath();
+      source = (path && path[0]) || event.target;
+    }
+
+    this.traceEvent(event, source);
+  }
+
+  traceEvent(event, source) {
+    if (!event) return;
+    const type = event.type;
+    const path = (event.composed && event.path) || (event.path = event.composedPath());
+    const target = (path && path[0]) || event.target;
+    !source || source === this
+      ? console.trace(`%O[%s] — %o`, target, type, event)
+      : console.trace(`%O - %O[%s] — %o`, target, source, type, event);
+  }
+
+  slotChangeCallback(event) {
+    traceEvent(event);
+  }
+
+  initialize() {}
+
+  connectedCallback() {
+    this.initialize();
+  }
+
+  set statusText(text) {
+    this['#status'] && (this['#status'].textContent = text);
+  }
+
+  get statusText() {
+    return this['#status'] && this['#status'].textContent;
+  }
+
+  set viewable(viewable) {
+    const wasViewable = this.wasViewable;
+    viewable = this.isViewable =
+      (viewable && (this.wasViewable || (this.wasViewable = true), true)) || false;
+    this.classList[this.isViewable ? 'add' : 'remove']('viewable');
+  }
+
+  get viewable() {
+    return this.isViewable || false;
+  }
+}
+
+/// HELPERS
+
+const zeroes = (value, length) => `${value || '0'}`.padStart((length > 0 && length) || 0, '0');
+
+const time = {
+  toString: (time = new Date()) =>
+    `${time.getMinutes()}:${zeroes(time.getSeconds(), 2)}.${zeroes(time.getMilliseconds(), 3)}`,
+};
