0a68a64dc8f7d8cc01f184e7da3efb3cfe7ad6e6
Saleh <saleh@grasppe.com>
2018-12-25 15:46:59 -0500

    Move modules to modules/alpha

--- modules/index.mjs
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node --experimental-modules
-
-console.clear();
-
-// import('../packages/modules.js');
-(async () => {
-  await import('./lib/modules.mjs');
-  await import('./specs/modules.js');
-})()
