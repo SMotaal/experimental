dbc934f66c869e4d092999b3f0a191bf50c9f837
Saleh <saleh@grasppe.com>
2019-01-29 15:10:39 -0500

    Add testing experimental

---
+++ testing/index.js
@@ -0,0 +1,28 @@
+﻿import {async, scope, mocha} from './modules/mocha.js';
+
+async(api => {
+	console.log(
+		'setup',
+		mocha.setup({
+			ui: 'tdd',
+			// ignoreLeaks: true,
+			asyncOnly: true,
+		}),
+	);
+
+	console.log(scope);
+
+	api.suite('sum', function() {
+		api.test('should return sum of arguments', async () => null);
+	});
+
+	mocha.checkLeaks();
+	mocha.run();
+
+	// console.log({mocha, namespace: {...namespace}});
+
+	// console.log({bdd: mocha.setup('bdd'), namespace: {...namespace}});
+	// const {describe, it} = mocha;
+});
+
+// (async () => void console.log(await (await import('./modules/mocha.js')).ready))();
---
+++ testing/modules/mocha.js
@@ -0,0 +1,54 @@
+﻿import loader from '../loader/loader.js';
+import packages from '../loader/packages.js';
+import {createNamespace, createEvaluator, createScriptProxy} from '../loader/helpers.js';
+
+/** @type {mocha} mocha */
+export let mocha;
+
+/** @type {mocha.globals} mocha */
+export const scope = createNamespace({
+	mocha: {get: () => mocha, set: value => void (mocha = value), enumerable: true},
+});
+
+export const ready = (scope.ready = (async module => {
+	await packages.ready;
+	const {main, stylesheet} = packages[module];
+	if (main) {
+		const url = packages[module].resolve(main);
+		const source = await loader.load(url);
+		const type = /^\s*export /m.test(source) ? 'module' : 'script';
+
+		stylesheet &&
+			typeof document === 'object' &&
+			(await new Promise((resolve, reject) => {
+				document.head.appendChild(
+					Object.assign(document.createElement('link'), {
+						rel: 'stylesheet',
+						href: url.replace(main, stylesheet),
+						onload: resolve,
+						onerror: reject,
+						onabort: resolve,
+					}),
+				);
+			}));
+
+		type === 'script'
+			? await createEvaluator(createScriptProxy(scope))(source)
+			: Object.assign(scope, await import(url));
+	}
+	return scope;
+})('mocha'));
+
+/**
+ * Defers testing until Mocha is loaded.
+ *
+ * @param {(scope: mocha.globals) => Promise<undefined>} ƒ
+ */
+export const async = async ƒ => ƒ(await ready);
+
+// export default scope;
+
+//* SEE: https://github.com/vitalets/mocha-es6-modules
+
+/** @typedef {typeof import('mocha')} mocha */
+/** @typedef {import('mocha').MochaGlobals & {mocha: mocha}} mocha.globals */
