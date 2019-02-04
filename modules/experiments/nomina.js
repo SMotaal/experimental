3184baef88815a265d99db4695e253e9794c57c4
Saleh Abdel Motaal <saleh@grasppe.com>
2019-02-03 20:29:20 -0500

    Add modules/experiments/nomina

---
+++ modules/experiments/nomina.js
@@ -0,0 +1,5 @@
+﻿import * as tokenizer from '../../markup/dist/tokenizer/tokenizer.mjs';
+import * as browser from '../../markup/dist/tokenizer/tokenizer.browser.mjs';
+import * as extended from '../../markup/dist/tokenizer/tokenizer.extended.mjs';
+
+console.log({tokenizer, browser, extended});
