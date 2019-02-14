f8a7d8bd6b562075c00fea87ddff15565d6273b0
Saleh <saleh@grasppe.com>
2019-02-14 18:35:47 -0500

    Update disambiguation.js

--- modules/experiments/disambiguation.js
+++ modules/experiments/disambiguation.js
@@ -1,5 +1,5 @@
-﻿import * as tokenizer from '../../markup/dist/tokenizer/tokenizer.mjs';
-import * as browser from '../../markup/dist/tokenizer/tokenizer.browser.mjs';
-import * as extended from '../../markup/dist/tokenizer/tokenizer.extended.mjs';
+﻿import * as tokenizer from '../../../markup/dist/tokenizer/tokenizer.mjs';
+import * as browser from '../../../markup/dist/tokenizer/tokenizer.browser.mjs';
+import * as extended from '../../../markup/dist/tokenizer/tokenizer.extended.mjs';
 
 console.log({tokenizer, browser, extended});
