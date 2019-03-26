d1b76d0a05e67a6efee9e8b56c5fe5f67a32f498
Saleh <saleh@grasppe.com>
2019-03-26 07:14:44 -0400

    Update tokenizer references

--- modules/experiments/disambiguation.js
+++ modules/experiments/disambiguation.js
@@ -1,5 +1,5 @@
 ﻿import * as tokenizer from '../../../markup/dist/tokenizer/tokenizer.mjs';
-import * as browser from '../../../markup/dist/tokenizer/tokenizer.browser.mjs';
-import * as extended from '../../../markup/dist/tokenizer/tokenizer.extended.mjs';
+import * as browser from '../../../markup/dist/tokenizer/tokenizer.browser.js';
+import * as extended from '../../../markup/dist/tokenizer/tokenizer.extended.js';
 
 console.log({tokenizer, browser, extended});
