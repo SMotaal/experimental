9245bc7f25f1c9582402b816679692612fbba4c2
Saleh <saleh@grasppe.com>
2019-03-29 16:33:14 -0400

    Update conres/targets/parser

--- conres/targets/parser.js
+++ conres/targets/parser.js
@@ -2,19 +2,45 @@
 
 const tokenizer = new Tokenizer();
 
-export const parse = source => {
-  const mode = {
-			// matcher: /([ \t]+)|([\s\n]+)|(.+?(?=\s|\n|$))|(.*)/gu,
-			// matcher: /([ \t]+)|([\s\n]+)|(.+?(?=\s|\n|$))/gu,
-			matcher: /([\t]+)|(\n+\s*)|([^\n\t]+)/g,
-			types: ['tabs', 'feed', 'sequence'],
-  };
-	const state = {
-		mode,
-	};
+const Normalizer = (() => {
+	class Normalizer extends RegExp {
+		replace(text, replacer = this.replacer) {
+			return !text || replacer == null ? text : this[Symbol.replace](text, replacer);
+		}
+	}
+
+	const Flags = /[yg]|$/;
+
+	return (matcher, replacer, flags) =>
+		Object.defineProperty(
+			new Normalizer(
+				(matcher && matcher.source) || matcher || undefined,
+				`${flags || (flags = matcher && matcher.flags) ? Flags[Symbol.replace](flags, 'g') : 'g'}`,
+			),
+			'replacer',
+			{value: replacer, enumerable: true},
+		);
+})();
+
+export const LineBreaks = Normalizer(/\n|\r\n|\r/g, '\n');
+export const Tabs = Normalizer(/\t/g);
+
+export const tokenize = source => {
+	const state = {mode};
 	const tokens = tokenizer.tokenize(source, state);
-
 	tokens.state = state;
-
 	return tokens;
+};
+
+export const parse = source => {
+	const tokens = tokenize(source);
+	const data = {};
+	return {data, tokens};
+};
+
+export const mode = {
+	matcher: / *(?:([\t]+)|((?:\n+|(?:\r\n)+|\r+))|((?:[-+]|\b)(?:\d*\.\d+|\d+(?:\.(?:\d*)|)|\d+)%?(?=\W|$)|(?:\d+E-?\d+|\d+\.\d+E-?\d+))|(\(\b\D\S*?(?:\b[.%]?|\b)\))|(\b(?:(?: *|\b *[-._/:,] *)\w.*?\b)+\S*|\S+?))/giu,
+
+	// |(\(?=.*?[\w\.]\))(\w[\w\. ]*\))
+	types: ['tabs', 'feed', 'numeric', 'unit', 'sequence'],
 };
--- conres/targets/parser.spec.js
+++ conres/targets/parser.spec.js
@@ -1,74 +1,91 @@
 ﻿import {loadSourceTextFrom} from './helpers.js';
-import {parse} from './parser.js';
+import {parse, tokenize, LineBreaks, Tabs} from './parser.js';
+
+const options = {
+	// benchmark: true,
+	tokenization: true,
+};
+
+setTimeout(async () => {
+	const sourceText = await loadSourceTextFrom('./examples/conres-35X-c.log');
+
+	benchmarks: if (options.benchmark) {
+		tokenization: if (options.tokenization) {
+			await new Promise(resolve => setTimeout(resolve, 1000));
+			group('Tokenization');
+			time('cool');
+			for (let n = 10; n--; all(tokenize(sourceText)));
+			timeEnd('cool');
+			time('warm');
+			for (let n = 10; n--; all(tokenize(sourceText)));
+			timeEnd('warm');
+			time('hot');
+			for (let n = 10; n--; all(tokenize(sourceText)));
+			timeEnd('hot');
+			groupEnd();
+		}
+	}
+
+	tokenizer: if (options.tokenization) {
+		const tokenizer = tokenize(sourceText);
+		const {state} = tokenizer;
+		const tokens = all(tokenizer);
+		group('Tokenization');
+		log('state: %o', state);
+		groupCollapsed(`Source ‹${typeof sourceText}›`);
+		log(
+			'%c%s',
+			'white-space: pre; tab-size: 20em; display: block; font-size: smaller;',
+			LineBreaks.replace(sourceText),
+		);
+		groupEnd();
+		groupCollapsed(`Output ‹tokens›`);
+		// log({tokenizer, state});
+		const output = {};
+		let nextIndex, skipped;
+		for (const token of tokens) {
+			const {index, text, type} = token;
+			if (index > nextIndex) {
+				output[nextIndex] = {skipped: sourceText.slice(nextIndex, index)};
+			}
+			output[index] = {type, text: renderWhitespace(text), skipped};
+			nextIndex = index + text.length;
+		}
+		table(output);
+		groupEnd();
+		groupEnd();
+	}
+});
+
+const {log, warn, group, groupCollapsed, groupEnd, table, time, timeEnd} = console;
+
+const renderWhitespace = text =>
+	text
+		? Tabs.replace(LineBreaks.replace(text, '¶'), '\u21E8')
+		: // .replace(/ /g, '\u2423')
+		  // .replace(/\t/g, '\u21E8')
+		  '';
 
 const dumpTokens = tokens => {
 	for (const token of tokens) {
-		const preview = token.text
-			.replace(/\n/g, '\u23CE')
-			.replace(/ /g, '\u2423')
-			.replace(/\t/g, '\u21E5');
+		const preview = renderWhitespace(token.text);
 		console.groupCollapsed(preview);
 		console.log(token);
 		console.groupEnd();
 	}
 };
 
-(async () => {
-	const sourceText = await loadSourceTextFrom('./examples/conres-35X-c.log');
-
-	await new Promise(resolve => setTimeout(resolve, 2000));
-
-	// const {from: all} = Array;
-	// const all = source => Array.from(source);
-	const all = {
-		from: Array.from,
-		spread: source => [...source],
-		forOf: source => {
-			const array = [];
-			for (const item of source) array.push(item);
-			return array;
-		},
-		for: source => {
-			const array = [];
-			for (let result = source.next(); result && !result.done; array.push(result.value), result = source.next());
-			return array;
-		},
-	}.from;
-
-	benchmark: {
-		console.time('cool');
-		// for (let n = 10; n--; [...parse(sourceText)]);
-		for (let n = 10; n--; all(parse(sourceText)));
-		console.timeEnd('cool');
-		console.time('warm');
-		for (let n = 10; n--; all(parse(sourceText)));
-		console.timeEnd('warm');
-		console.time('hot');
-		for (let n = 10; n--; all(parse(sourceText)));
-		console.timeEnd('hot');
-	}
-
-	spec: {
-		// console.log(sourceText);
-		const tokenizer = parse(sourceText);
-		const {state} = tokenizer;
-		const tokens = all(tokenizer);
-		console.group(sourceText);
-		console.log('state: %o', state);
-		// console.log({tokenizer, state});
-		const table = {};
-		for (const token of tokens) {
-			const {index, text, type} = token;
-			table[index] = {
-				text: text
-					.replace(/\n/g, '\u23CE')
-					.replace(/ /g, '\u2423')
-					.replace(/\t/g, '\u21E5'),
-				type,
-			};
-			// token.text = ;
-		}
-		console.table(table);
-		console.groupEnd();
-	}
-})();
+const all = {
+	from: Array.from,
+	spread: source => [...source],
+	forOf: source => {
+		const array = [];
+		for (const item of source) array.push(item);
+		return array;
+	},
+	for: source => {
+		const array = [];
+		for (let result = source.next(); result && !result.done; array.push(result.value), result = source.next());
+		return array;
+	},
+}.from;
