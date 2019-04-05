4a19356b233212330e86ca5a9064d052961b9edc
Saleh <saleh@grasppe.com>
2019-04-05 14:32:49 -0400

    Add resolver experiment (prelim globs work)

---
+++ resolver/globs.spec.js
@@ -0,0 +1,32 @@
+﻿const currentScript = document['--currentScript--'] || document.currentScript;
+
+(async () => {
+	const {default: spec} = await import(new URL(
+		'./lib/globs.spec.js',
+		(currentScript && currentScript.src) || location,
+	));
+
+	const logger = {...console};
+	try {
+		const {parentElement} = currentScript;
+		if (parentElement) {
+			logger.dir = (value, options) => {
+				parentElement
+					.appendChild(document.createElement('pre'))
+					.appendChild(document.createElement('code'))
+					.appendChild(new Text(value));
+			};
+
+			logger.table = object => {
+				logger.dir(JSON.stringify(object, null, 2).replace(/\\\\/g, '\\'));
+			};
+			logger.log = logger.table;
+		}
+	} finally {
+		try {
+			spec({logger});
+		} catch (exception) {
+			logger.dir(exception);
+		}
+	}
+})().catch(console.warn);
