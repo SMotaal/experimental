87dfb158404e4fb2a61870bf62189172bf6cf81c
Saleh <saleh@grasppe.com>
2019-04-05 14:32:49 -0400

    Add resolver experiment (prelim globs work)

---
+++ resolver/lib/globs.js
@@ -0,0 +1,54 @@
+﻿export const PathExpression = (source, flags = source.flags) =>
+	new RegExp(
+		(((source || '') && source.source) || source).replace(
+			/\[\^\/\](?=[*+?])/g,
+			PathExpression.SegmentParts.source.slice(0, -1),
+		),
+		flags,
+	);
+
+PathExpression.SegmentParts = /(?:\[(?:\[|\]|(?:\\.|[^\\])*?)]\|\\.|\\$|[^\\/]+)+/g;
+PathExpression.DoubleAstrisksParts = PathExpression(/(^|\/)[^/]*\*\*[^/]*(?:\/\.\.|\/\.)*(?=\/|$)/g);
+PathExpression.DotSlashParts = PathExpression(/(^|\/)\.(?:\/|$)/g);
+PathExpression.DoubleDotSlashParts = PathExpression(/(^|\/?)(?:(?=[^/])[^/]+\/|)\.\.(?:\/|$)/g);
+PathExpression.PathSegments = PathExpression(/^(?=\/+)|(?=[^/])[^/]+/g);
+
+const matchAll = (string, pattern) => {
+	pattern = new RegExp((pattern && pattern.source) || pattern, pattern && 'flags' in pattern ? pattern.flags : 'g');
+	if (!/[gy]/.test(pattern.flags)) return (pattern.exec(string) || '')[0];
+	let match;
+	const results = [];
+	while ((match = pattern.exec(string))) results.push(match[0]);
+	return results;
+};
+
+PathExpression.split = path => [
+	...((path || '') &&
+		`${path}`
+			.replace(PathExpression.DoubleAstrisksParts, '$1**')
+			.replace(PathExpression.DotSlashParts, '$1')
+			.replace(PathExpression.DoubleDotSlashParts, '$1')
+			.matchAll(PathExpression.PathSegments)),
+];
+
+PathExpression.normalize = path => PathExpression.split(path).join('/');
+
+PathExpression.fromGlob = glob => {
+	const normlized = PathExpression.split(glob).join('/');
+	return new RegExp(
+		`^${
+			normlized
+				// .replace(/(^|[^\\])\[(\!|)(\[|\]|[^\[\]\\/]+?)\]/g, (m, a, b, c) =>
+				.replace(/\\.|\[(\^?)(\[|\]|[^\[\]\\/]*?)\]|(\?)/g, (m, b, c, d) =>
+					d ? `[^/]` : b || c ? `(?=[^\\\\/])[${b ? '^' : ''}${c === '[' || c === ']' ? `\\${c}` : c}]` : m,
+				)
+				// .replace(/(^|\/)\*\*($|\/)/g, `$1(?:${PathExpression.SegmentParts.source}/)*?`)
+				// .replace(/(^|\/)\*\*($|\/)/g, '$1(?:.*$2|)')
+				// .replace(/(^|\/)\*\*($|\/)/g, '(?:^|\\/).*(?:$|\\/)')
+				.replace(/(^|\/)\*\*($|\/)/g, (m, a, b) => (a ? `/(?:.*(?:$|/))?` : b ? `(?:(?:^|/).*)?/` : m))
+				.replace(/\*/g, '.*')
+			// .replace(/(^|[^\*])\*($|[^\*])/g, `$1${PathExpression.SegmentParts.source}$2`)
+			// .replace(/(^|\/)\*\*($|\/)/g, `$1.*$2`)
+		}`,
+	);
+};
---
+++ resolver/lib/globs.spec.js
@@ -0,0 +1,47 @@
+﻿import {PathExpression} from './globs.js';
+
+export default ({logger = console} = {}) => {
+	const paths = [`a/./b/?/c/../c/d/../d/e`]
+		.flatMap(path => [
+			path,
+			(path = path.replace('/b/', '/b/**/../')),
+			(path = path.replace('/c/', '/c[123][1-3][^1-3][[]/*/')),
+			(path = path.replace('/e', '/*/e*/f')),
+		])
+		.flatMap(path => [
+			(path = path.replace(/^\/?/, '')),
+			(path = `../${path}`),
+			(path = `/${path}`),
+			(path = path.replace(/\/?$/, '/*')),
+			(path = path.replace(/^\/?/, '**/')),
+			(path = `/${path}`),
+		]);
+
+	const results = {};
+	const rows = [];
+
+	for (const source of paths) {
+		const glob = PathExpression.normalize(source);
+		const matcher = PathExpression.fromGlob(glob);
+		const pass = [];
+		const fail = [];
+		const base = glob
+			.replace(/\\.|\[(\^?)(\[|\]|[^\[\]\\/]*?)\]/g, (m, b, c) => (b || c ? `${b ? '×' : c[0]}` : m))
+			.replace(/\?/g, '$');
+
+		let spec;
+
+		(matcher.test((spec = base.replace(/(^|\/)\*\*($|\/)/g, '/').replace(/\*/g, '×'))) ? pass : fail).push(spec),
+			(matcher.test((spec = spec.replace('/b/', '/'))) ? fail : pass).push(spec);
+		(matcher.test((spec = base.replace(/(^|\/)\*\*($|\/)/g, '$1××/××$2').replace(/\*/g, '×'))) ? pass : fail).push(
+			spec,
+		),
+			(matcher.test((spec = spec.replace('/b/', '/'))) ? fail : pass).push(spec);
+
+		rows.push((results[source] = {source, glob, specs: {pass, fail}}));
+	}
+
+	logger && typeof logger.table === 'function' && logger.table(rows);
+
+	return results;
+};
